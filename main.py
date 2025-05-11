8d247fdb158acc5235ab89a5ba3818b950484d6c
kubosis <kubasukdol@gmail.com>
2025-05-11 22:48:00 +0200

    [refactor] move models to separate file

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from torchmetrics import Accuracy
 import os
 
 from mlp_visualizer import ModelCollector, visualize_mlp
+from models import *
 
 import argparse
 
@@ -51,144 +52,6 @@ parser.add_argument(
     help="Optional path to save the collected data.",
 )
 
-
-
-class ImageClassificationBase(nn.Module):
-    def training_step(self, batch):
-        images, labels = batch
-        out = self(images)  # Generate predictions
-        loss = F.cross_entropy(out, labels)  # Calculate loss
-        return loss
-
-    def validation_step(self, batch):
-        images, labels = batch
-        out = self(images)  # Generate predictions
-        loss = F.cross_entropy(out, labels)  # Calculate loss
-        acc = accuracy(out, labels)  # Calculate accuracy
-        return {'val_loss': loss.detach(), 'val_acc': acc}
-
-    def validation_epoch_end(self, outputs):
-        batch_losses = [x['val_loss'] for x in outputs]
-        epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
-        batch_accs = [x['val_acc'] for x in outputs]
-        epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
-        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
-
-    def epoch_end(self, epoch, result):
-        print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
-            epoch, result['train_loss'], result['val_loss'], result['val_acc']))
-
-
-def accuracy(outputs, labels):
-    _, preds = torch.max(outputs, dim=1)
-    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
-
-
-class Cifar10CnnModel(ImageClassificationBase):
-    def __init__(self):
-        super().__init__()
-        self.network = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),  # output: 64 x 16 x 16
-
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),  # output: 128 x 8 x 8
-
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),  # output: 256 x 4 x 4
-
-            nn.Flatten(),
-            nn.Linear(256 * 4 * 4, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10))
-
-    def forward(self, xb):
-        return self.network(xb)
-
-
-class CNN_large(nn.Module):
-    def __init__(self):
-        super(CNN_large, self).__init__()
-        self.conv_block = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-
-        self.linear_block = nn.Sequential(
-            nn.Dropout(p=0.5),
-            nn.Linear(128 * 7 * 7, 128),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(128, 64),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(64, 10)
-        )
-
-    def forward(self, x):
-        x = self.conv_block(x)
-        x = x.view(x.size(0), -1)
-        x = self.linear_block(x)
-        return x
-
-
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(64 * 3*3, 24)
-        self.fc2 = nn.Linear(24, 24)
-        self.fc3 = nn.Linear(24, 24)
-        self.fc4 = nn.Linear(24, 24)
-        self.fc5 = nn.Linear(24, 10)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = F.relu(F.max_pool2d(self.conv3(x), 2))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = F.relu(F.max_pool2d(self.conv4(x), 2))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = x.view(-1, 64 * 3*3)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = F.relu(self.fc2(x))
-        x = F.dropout(x, training=self.training)
-        x = F.relu(self.fc3(x))
-        x = F.dropout(x, training=self.training)
-        x = F.relu(self.fc4(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc5(x)
-        return x
-
-
 def train_and_collect(train_dataset, test_dataset, batch_size=64, epochs=1,
                       lr=0.002, seed=1,
                       data_collection_interval=50, num_collections=-1, path="./collection.json",
@@ -229,7 +92,7 @@ def train_and_collect(train_dataset, test_dataset, batch_size=64, epochs=1,
     collector = ModelCollector(model, neuron_cap)
 
     acc = Accuracy("multiclass", num_classes=10).to(device)
-
+    avg_loss = 0
     for epoch in range(epochs):
         model.train()
         for batch_idx, (data, target) in enumerate(train_loader):
@@ -269,7 +132,7 @@ def train_and_collect(train_dataset, test_dataset, batch_size=64, epochs=1,
                 collector.register_value("label", prediction_label)
                 collector.register_value("logits", output[0])
                 accuracy = acc.compute()
-                collector.register_value("loss", loss.item())
+                collector.register_value("loss", avg_loss / batch_idx + 1 + epoch * len(train_loader))
                 collector.register_value("accuracy", accuracy)
                 print(f"Loss: {loss.item():.6f}, Accuracy: {accuracy:.2f}")
 
